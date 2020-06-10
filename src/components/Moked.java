5ce1747babfbea674877c2398075a6900eec3528
itay9 <itaydali@gmail.com>
2020-06-10 14:06:58 +0300

    Moked need to fix time from start

--- src/components/Moked.java
+++ src/components/Moked.java
@@ -2,13 +2,14 @@
 package components;
 
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class Moked {
     private int reportCounter;
     private FileWriter out; //output
-    public Moked(){
+    public Moked() throws IOException {
         reportCounter = 0;
-        out = null;
+        out = new FileWriter("out.txt");
     }
 
     public FileWriter getOut() {
@@ -31,5 +32,8 @@ public class Moked {
         String string = "Car number "+String.valueOf(vehicle.getID())+" got a ticket, file location: '"+out.toString()+"'";
         return string;
     }
-
+    public void giveReport(Vehicle vehicle){
+        out.write("time: "+TIME+", vehicle number: "+String.valueOf(vehicle.getID())+", #"+(++reportCounter));
+        System.out.println(reportMSG(vehicle));
+    }
 }
