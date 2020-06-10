110695c8300e86ab37101ee35cdcece8d4afe3cb
itay9 <itaydali@gmail.com>
2020-06-10 11:18:26 +0300

    Moked

--- src/components/Moked.java
+++ src/components/Moked.java
@@ -5,9 +5,31 @@ import java.io.FileWriter;
 
 public class Moked {
     private int reportCounter;
-    private FileWriter out;
+    private FileWriter out; //output
     public Moked(){
         reportCounter = 0;
         out = null;
     }
+
+    public FileWriter getOut() {
+        return out;
+    }
+
+    public int getReportCounter() {
+        return reportCounter;
+    }
+    public void incCount(){
+        reportCounter++;
+    }
+
+    /**
+     *
+     * @param vehicle vehicle that got speeding ticket
+     * @return string of msg to print
+     */
+    public String reportMSG(Vehicle vehicle){
+        String string = "Car number "+String.valueOf(vehicle.getID())+" got a ticket, file location: '"+out.toString()+"'";
+        return string;
+    }
+
 }
