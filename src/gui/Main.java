2f9b4ac97ce0b0f2f70812f3468a3310161427e1
Itay9 <itaydali@gmail.com>
2020-06-19 15:45:10 +0300

    reports

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -6,30 +6,31 @@ import javax.swing.*;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 
 public class Main extends JFrame implements ActionListener {
-   private static final long serialVersionUID = 1L;
-   private RoadSystemPanel panel;
-   private String[] names = {"Exit","Blue","None","Blue","Magenta","Orange", "Random","Help"};
-   private JMenu m1, m2, m3, m4, m5, m6, m7;
-   private JMenuItem[] mi;
-   private JMenuBar mb;
+	private static final long serialVersionUID = 1L;
+	private RoadSystemPanel panel;
+	private String[] names = {"Exit", "Blue", "None", "Blue", "Magenta", "Orange", "Random", "Help"};
+	private JMenu m1, m2, m3, m4, m5, m6, m7;
+	private JMenuItem[] mi;
+	private JMenuBar mb;
 
-   
-   public static void main(String[]args) {
-	   Main fr = new Main();
-	   fr.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-	   fr.setSize(845,715);
-	   fr.setVisible(true);
-   }
 
-   
-   public Main() {
-	    super("Road system");
-	    panel = new RoadSystemPanel(this);
-	    add(panel);
-	    panel.setVisible(true);
+	public static void main(String[] args) {
+		Main fr = new Main();
+		fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fr.setSize(845, 715);
+		fr.setVisible(true);
+	}
+
+
+	public Main() {
+		super("Road system");
+		panel = new RoadSystemPanel(this);
+		add(panel);
+		panel.setVisible(true);
 
 		mb = new JMenuBar();
 		m1 = new JMenu("File");
@@ -41,9 +42,9 @@ public class Main extends JFrame implements ActionListener {
 		m7 = new JMenu("Reports");
 		mi = new JMenuItem[names.length];
 
-		for(int i=0;i<names.length;i++) {
-		    mi[i]=new JMenuItem(names[i]);
-		    mi[i].addActionListener(this);
+		for (int i = 0; i < names.length; i++) {
+			mi[i] = new JMenuItem(names[i]);
+			mi[i].addActionListener(this);
 		}
 
 		m1.add(mi[0]);
@@ -60,9 +61,9 @@ public class Main extends JFrame implements ActionListener {
 		m3.add(mi[5]);
 		m3.addSeparator();
 		m3.add(mi[6]);
-		
+
 		m4.add(mi[7]);
-		
+
 //		m5.add(mi[?]);
 		m5.addActionListener(this);
 
@@ -80,60 +81,67 @@ public class Main extends JFrame implements ActionListener {
 		mb.add(m5);
 		mb.add(m6);
 		mb.add(m7);
-		
+
 		setJMenuBar(mb);
-   }
-   
+	}
+
 
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() == mi[0])
+		if (e.getSource() == mi[0])
 			destroy();
-		else if(e.getSource() == mi[1])
+		else if (e.getSource() == mi[1])
 			panel.setBackgr(1);
-		else if(e.getSource() == mi[2])
+		else if (e.getSource() == mi[2])
 			panel.setBackgr(0);
-		else if(e.getSource() == mi[3])
+		else if (e.getSource() == mi[3])
 			panel.setColorIndex(0);
-		else if(e.getSource() == mi[4])
+		else if (e.getSource() == mi[4])
 			panel.setColorIndex(1);
-		else if(e.getSource() == mi[5])
+		else if (e.getSource() == mi[5])
 			panel.setColorIndex(2);
-		else if(e.getSource() == mi[6])
+		else if (e.getSource() == mi[6])
 			panel.setColorIndex(3);
-		else if(e.getSource() == mi[7])
+		else if (e.getSource() == mi[7])
 			printHelp();
-		else if(e.getSource() == m5)
+		else if (e.getSource() == m5)
 			BuildMap();
-		else if(e.getSource() == m6)
+		else if (e.getSource() == m6)
 			CloneCar();
-		else if(e.getSource() == m7)
+		else if (e.getSource() == m7)
 			Reports();
-		
+
 	}
-	
-	
+
+
 	public void destroy() {
 		System.exit(0);
 	}
-	
-	
+
+
 	public void printHelp() {
 		JOptionPane.showMessageDialog(this, "Home Work 3\nGUI @ Threads");
 	}
-	
+
 	public void BuildMap() {
 		//TODO: build a map button
 	}
-	
+
 	public void CloneCar() {
-		//TODO: panel to choose car ID for cloning
-		int index; //
+		int index;  //TODO: panel to choose car ID for cloning
 		Vehicle vehicle = panel.getDriving().getVehicles().get(index).clone();
 		panel.getDriving().addVehicle(vehicle); // adding to list of vehivles
 	}
-	
-	public void Reports() {
-		//TODO: report button
-	}
 
+	public void Reports() {
+		Runtime load = Runtime.getRuntime();
+		// todo String file = "C:\\Users\\Almog\\eclipse-workspace\\Roads\\report.txt";
+		//todo chen edit for your file
+		String fileName = "E:\\Programing\\GitHub\\AOOP2020_HW4\\reports.txt";
+		try {
+			Process p = load.exec("notepad " + fileName);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
 }
